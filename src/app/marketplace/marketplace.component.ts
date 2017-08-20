02413821f1275baac7fe1d1a81ce867b374519f7
Nicole Freed <nicole.freed@gmail.com>
2017-08-20 16:28:54 -0700

    create ngOnInit() method to load albums automatically

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -17,11 +17,11 @@ export class MarketplaceComponent implements OnInit {
 
   }
 
+  ngOnInit() {
+    this.albums = this.albumService.getAlbums();
+  }
+
   goToDetailPage(clickedAlbum: Album) {
     this.router.navigate(['albums', clickedAlbum.id]);
   };
-
-  ngOnInit() {
-  }
-
 }
