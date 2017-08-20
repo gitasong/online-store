6dae34a5d037899d017188920425fbc1d69431d1
Nicole Freed <nicole.freed@gmail.com>
2017-08-20 16:33:47 -0700

    declare AlbumService as provider in @Component decorator

--- src/app/album-detail/album-detail.component.ts
+++ src/app/album-detail/album-detail.component.ts
@@ -7,7 +7,8 @@ import { AlbumService } from '../album.service';
 @Component({
   selector: 'app-album-detail',
   templateUrl: './album-detail.component.html',
-  styleUrls: ['./album-detail.component.css']
+  styleUrls: ['./album-detail.component.css'],
+  providers: [AlbumService]
 })
 
 export class AlbumDetailComponent implements OnInit {
