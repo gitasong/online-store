8d6d00fc13086d53115cf1a4ebe53a4689e537aa
Nicole Freed <nicole.freed@gmail.com>
2017-08-20 10:52:12 -0700

    create method goToDetailPage() in marketplace component

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -18,9 +18,13 @@ export class MarketplaceComponent implements OnInit {
       "Her highest-selling and most acclaimed album, released in 1996.", 4),
     new Album("Chopin - Complete Nocturnes", "Brigitte Engerer",
       "Released in 2010, this is Engerer's own rendition of the classical composer Chopin.", 5),
-    new Album("Axis Bold As Love", "The Jimi Hendrix Experience", 
+    new Album("Axis Bold As Love", "The Jimi Hendrix Experience",
       "Second studio album by the English-American band, released in 1967.", 6)
- ];
+  ];
+
+  goToDetailPage(clickedAlbum: Album) {
+    this.router.navigate(['albums', clickedAlbum.id]);
+  };
 
   constructor() { }
 
