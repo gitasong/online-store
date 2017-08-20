5c2b38ec9ef3f4b6568cf114f52e698981bc69fd
Nicole Freed <nicole.freed@gmail.com>
2017-08-20 16:22:32 -0700

    import album service

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Album } from '../album.model';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-marketplace',
