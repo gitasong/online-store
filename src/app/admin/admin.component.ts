37298e375a12b19429128db2de798912d707443a
Nicole Freed <nicole.freed@gmail.com>
2017-08-23 17:51:39 -0700

    import album service

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlbumService } from '../album.service';
 import { Album } from '../album.model';
 
 @Component({
