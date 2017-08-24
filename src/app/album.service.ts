154356aa7b2686078453655e777e4d32ba884419
Nicole Freed <nicole.freed@gmail.com>
2017-08-23 17:57:13 -0700

    create addAlbum() method

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -15,6 +15,10 @@ export class AlbumService {
     return this.albums;
   }
 
+  addAlbum(newAlbum: Album) {
+    this.albums.push(newAlbum);
+  }
+
   // getAlbumById(albumId: number){
   //   for (var i = 0; i <= ALBUMS.length - 1; i++) {
   //     if (ALBUMS[i].id === albumId) {
