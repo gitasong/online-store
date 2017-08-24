5448219223ac5731d559eef527c7604797c82891
Nicole Freed <nicole.freed@gmail.com>
2017-08-23 17:10:12 -0700

    create submitForm() method to create new album upon form submission

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -12,4 +12,9 @@ export class AdminComponent implements OnInit {
   ngOnInit() {
   }
 
+  submitForm(title: string, artist: string, description: string) {
+    var newAlbum: Album = new Album(title, artist, description);
+    console.log(newAlbum);
+  }
+
 }
