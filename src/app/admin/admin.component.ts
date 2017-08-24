0657f481ae9b95e10522e8042b1ff522229e5a1f
Nicole Freed <nicole.freed@gmail.com>
2017-08-23 17:11:25 -0700

    add new album properties to constructor

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(public title: string, public artist: string, public description: string) { }
 
   ngOnInit() {
   }
